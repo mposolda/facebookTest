b693c46d63eb9ad68e606cce312349e012bea16e
mposolda <mposolda@gmail.com>
2013-04-09 15:27:12 +0200

    Added dependency on restFB. Showing list of friends

---
+++ src/main/java/org/jboss/facebook/restfb/FacebookServlet3.java
@@ -0,0 +1,105 @@
+package org.jboss.facebook.restfb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.restfb.Connection;
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.Parameter;
+import com.restfb.types.FriendList;
+import com.restfb.types.NamedFacebookType;
+import com.restfb.types.User;
+
+/**
+ * @author <a href="mailto:mposolda@redhat.com">Marek Posolda</a>
+ */
+public class FacebookServlet3 extends HttpServlet {
+
+    // "email"
+    private static final String MY_ACCESS_TOKEN1 = "AAACEdEose0cBAK0kRFp9Ycwu2nbT3xuE0ei0BzGUqwKYeCpvNcgnGFwM3fRz5MPAncPteqs1cmqqPcR0FEgzZAUx3eQWYxN8USnvoiTFrORK2NYZBZA";
+    private static final int ITEMS_PER_PAGE = 10;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        System.out.println("FBS3: doGet for URL " + req.getRequestURI());
+        resp.setContentType("text/html");
+        HttpSession session = req.getSession();
+        PrintWriter out = resp.getWriter();
+
+        FacebookClient facebookClient = new DefaultFacebookClient(MY_ACCESS_TOKEN1);
+        User user = facebookClient.fetchObject("me", User.class);
+        out.println("User: " + user.getName() + "<br>");
+        out.println("username: " + user.getUsername() + "<br>");
+        out.println("email: " + user.getEmail() + "<br>");
+        out.println("<hr>");
+
+        // Count total number of friends, TODO: cache
+        Integer friendsCount = (Integer)session.getAttribute("friendsCount");
+        if (friendsCount == null) {
+            Connection<NamedFacebookType> myFriends = facebookClient.fetchConnection("me/friends", NamedFacebookType.class);
+            friendsCount = myFriends.getData().size();
+            session.setAttribute("friendsCount", friendsCount);
+        }
+
+        Integer pageNumber;
+        if (req.getParameter("page") != null) {
+            pageNumber = Integer.parseInt(req.getParameter("page"));
+            session.setAttribute("page", pageNumber);
+        } else {
+            pageNumber = (Integer)session.getAttribute("page");
+        }
+        if (pageNumber == null) {
+            pageNumber = 1;
+        }
+
+        Integer pageCount = ((friendsCount-1) / ITEMS_PER_PAGE) + 1;
+        Integer indexStart = (pageNumber - 1) * ITEMS_PER_PAGE;
+        List<NamedFacebookType> friendsToDisplay = facebookClient.fetchConnection("me/friends", NamedFacebookType.class, Parameter.with("offset", indexStart), Parameter.with("limit", ITEMS_PER_PAGE)).getData();
+
+
+        out.println("Count of friends: " + friendsCount + "<br>");
+        out.println("Page: " + pageNumber + "<br>");
+        out.println("Select page: ");
+        for (int i=1 ; i<=pageCount ; i++) {
+            String url = req.getContextPath() + req.getServletPath() + "?page=" + i;
+            out.print("<a href=\"" + url + "\">" + i + "</a> ");
+        }
+        out.println("<br>");
+
+
+
+        // TODO: performance
+        for (NamedFacebookType current : friendsToDisplay) {
+            UserWithPicture friendWithPicture = facebookClient.fetchObject(current.getId(), UserWithPicture.class, Parameter.with("fields", "id,name,picture"));
+            String urlForPersonDetail = req.getContextPath() + req.getServletPath() + "?friendId=" + friendWithPicture.getId();
+            out.println("<a href=\"" + urlForPersonDetail + "\"><img src=\"" + friendWithPicture.getPicture().getData().getUrl() + "\" title=\"" +  friendWithPicture.getName() + "\" /></a>");
+        }
+        out.println("<hr>");
+
+        String friendId = req.getParameter("friendId");
+        if (friendId != null) {
+            User friend = facebookClient.fetchObject(friendId, User.class);
+            out.println("Friend name: " + friend.getName());
+
+
+        }
+
+        System.out.println("end");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        System.out.println("FBS3: doPost");
+        doGet(req, resp);
+    }
+}
